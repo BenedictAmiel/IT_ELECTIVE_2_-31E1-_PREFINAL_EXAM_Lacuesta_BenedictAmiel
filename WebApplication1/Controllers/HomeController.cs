03be93d9247cd011251d3c74911c45c795a8ae24
BenedictAmiel <benedictlacuesta@gmail.com>
2026-09-12 11:12:38 +0800

    added question 7

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -87,6 +87,19 @@ public class HomeController : Controller
                 },
                 ChosenAnswer = "B"
             },
+            new ExamQuestion
+            {
+                Number = 7,
+                Question = "A Student belongs to exactly one Section, while a Section can contain many students. What type of relationship is this?",
+                Choices = new List<string>
+                {
+                    "A. One-to-One",
+                    "B. One-to-Many",
+                    "C. Many-to-Many",
+                    "D. Many-to-One only"
+                },
+                ChosenAnswer = "B"
+            },
         };
         return View(questions);
     }
