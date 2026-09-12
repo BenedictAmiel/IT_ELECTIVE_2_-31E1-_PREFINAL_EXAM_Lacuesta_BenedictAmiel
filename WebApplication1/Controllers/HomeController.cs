00f7228188b4a00dc4c0547fa3175c907d95bd0d
BenedictAmiel <benedictlacuesta@gmail.com>
2026-09-12 11:10:17 +0800

    added question 5

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -61,6 +61,19 @@ public class HomeController : Controller
                 },
                 ChosenAnswer = "A"
             },
+            new ExamQuestion
+            {
+                Number = 5,
+                Question = "What does the following command primarily do?\ndotnet ef dbcontext scaffold \"ConnectionString\" Microsoft.EntityFrameworkCore.SqlServer -o Models",
+                Choices = new List<string>
+                {
+                    "A. Deletes the database",
+                    "B. Creates a new MVC project",
+                    "C. Generates EF Core models and a DbContext from an existing database",
+                    "D. Starts the MVC application"
+                },
+                ChosenAnswer = "C"
+            },
         };
         return View(questions);
     }
