72048a5eb12ea0f4862f95b7df8f02964e542462
BenedictAmiel <benedictlacuesta@gmail.com>
2026-09-12 10:51:20 +0800

    added question 1

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        var questions = new List<ExamQuestion>
+        {
+            new ExamQuestion
+            {
+                Number = 1,
+                Question = "What is the main problem solved by using a database instead of an in-memory collection?",
+                Choices = new List<string>
+                {
+                    "A. It makes C# code shorter",
+                    "B. It prevents the application from restarting",
+                    "C. It allows data to persist after the application stops",
+                    "D. It removes the need for MVC"
+                },
+                ChosenAnswer = "C"
+            },
+            
+        };
+        return View(questions);
+    }
+}
